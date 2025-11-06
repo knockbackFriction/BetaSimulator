c6532c0a7990e64c24f09128214339985ecec3d5
knockbackFriction <234834046+knockbackFriction@users.noreply.github.com>
2025-11-06 09:23:09 +0100

    old armor protection (todo fix incompat with new sword/axe damage)

--- src/main/java/eu/anon/betasimulator/DamageChanger.java
+++ src/main/java/eu/anon/betasimulator/DamageChanger.java
@@ -6,6 +6,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class DamageChanger implements Listener {
     boolean extraBaseDmg = BetaSimulator.instance.configuration.barehandDamageFullHeart;
@@ -46,5 +48,87 @@ public class DamageChanger implements Listener {
         }
 
         event.setDamage(damage);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+        Player player = (Player) event.getEntity();
+
+        float dmgReduction = 0.0f;
+
+        for (ItemStack armorPiece : player.getEquipment().getArmorContents()) {
+            short maxDura = -1;
+
+            if (armorPiece != null) {
+                switch (armorPiece.getType()) {
+                    case LEATHER_BOOTS:
+                        maxDura = 65;
+                        break;
+                    case LEATHER_CHESTPLATE:
+                        maxDura = 80;
+                        break;
+                    case LEATHER_HELMET:
+                        maxDura = 55;
+                        break;
+                    case LEATHER_LEGGINGS:
+                        maxDura = 75;
+                        break;
+                    case CHAINMAIL_BOOTS:
+                    case IRON_BOOTS:
+                        maxDura = 195;
+                        break;
+                    case CHAINMAIL_CHESTPLATE:
+                    case IRON_CHESTPLATE:
+                        maxDura = 240;
+                        break;
+                    case CHAINMAIL_HELMET:
+                    case IRON_HELMET:
+                        maxDura = 165;
+                        break;
+                    case CHAINMAIL_LEGGINGS:
+                    case IRON_LEGGINGS:
+                        maxDura = 225;
+                        break;
+                    case GOLD_BOOTS:
+                        maxDura = 91;
+                        break;
+                    case GOLD_CHESTPLATE:
+                        maxDura = 112;
+                        break;
+                    case GOLD_HELMET:
+                        maxDura = 77;
+                        break;
+                    case GOLD_LEGGINGS:
+                        maxDura = 105;
+                        break;
+                    case DIAMOND_BOOTS:
+                        maxDura = 429;
+                        break;
+                    case DIAMOND_CHESTPLATE:
+                        maxDura = 528;
+                        break;
+                    case DIAMOND_HELMET:
+                        maxDura = 363;
+                        break;
+                    case DIAMOND_LEGGINGS:
+                        maxDura = 495;
+                        break;
+                }
+            }
+
+            if (maxDura != -1) {
+                short dura = armorPiece.getDurability();
+                dmgReduction += ( (float) (maxDura - dura) / (float) maxDura ) * -0.25f;
+            }
+        }
+
+        //we need to fix incompatibilities with the new sword and axe damages
+        if (dmgReduction < 0.0f) {
+            event.setDamage(
+                    EntityDamageEvent.DamageModifier.ARMOR,
+                    dmgReduction * event.getDamage()
+            );
+        }
     }
 }
